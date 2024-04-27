6814d0b9ffff81f158c17762bd7ccddcb4b77cb5
DanTheMan <52235296+danielstarodubtsev@users.noreply.github.com>
2024-04-27 14:19:07 +0300

    fix

--- map.py
+++ map.py
@@ -13,41 +13,30 @@ class Map:
     self._food_count = 0
     self._big_food_count = 0
     self._stop_food_count = 0
+    self._wall_count = 0
 
   def get_cell(self, x: int, y: int) -> Cell:
     return self._map[y][x]
   
+  def _place_object(self, snake1: Snake, snake2: Snake, object_name: str, object_type: 
+                    int, max_count: int, probability: float) -> None:
+    if chance(probability):
+      for _ in range(10): # Limit the number of attempts
+        x, y = random_coords(Map.SIZE - 1)
+        if self._map[y][x].value() == Cell.EMPTY and (x, y) not in snake1.body \
+           and (x, y) not in snake2.body:
+          self._map[y][x].set_value(object_type)
+          count_attribute = getattr(self, f'_{object_name}_count')
+          setattr(self, f'_{object_name}_count', count_attribute + 1)
+          break
+
   def perform_spawns(self, first_snake: Snake, second_snake: Snake) -> None:
-    # Я не буду объединять это в один метод, т.к. код внутри if-ов принципиально различается и меняет
-    # разные локальные переменные
+    spawns = [
+      ("food", Cell.HAS_FOOD, MaxSpawn.FOOD, Probabilities.FOOD_SPAWN),
+      ("big_food", Cell.HAS_BIG_FOOD, MaxSpawn.BIG_FOOD, Probabilities.BIG_FOOD_SPAWN),
+      ("stop_food", Cell.HAS_STOP_FOOD, MaxSpawn.STOP_FOOD, Probabilities.STOP_FOOD_SPAWN),
+      ("wall", Cell.HAS_WALL, 1, Probabilities.WALL_SPAWN)
+    ]
 
-    if chance(Probabilities.FOOD_SPAWN) and self._food_count < MaxSpawn.FOOD:
-      while True:
-        x, y = random_coords(Map.SIZE - 1)
-        if self._map[y][x].value() == Cell.EMPTY and (x, y) not in first_snake.body and (x, y) not in second_snake.body:
-          self._map[y][x].set_value(Cell.HAS_FOOD)
-          self._food_count += 1
-          break
-
-    if chance(Probabilities.BIG_FOOD_SPAWN) and self._big_food_count < MaxSpawn.BIG_FOOD:
-      while True:
-        x, y = random_coords(Map.SIZE - 1)
-        if self._map[y][x].value() == Cell.EMPTY and (x, y) not in first_snake.body and (x, y) not in second_snake.body:
-          self._map[y][x].set_value(Cell.HAS_BIG_FOOD)
-          self._big_food_count += 1
-          break
-
-    if chance(Probabilities.STOP_FOOD_SPAWN) and self._stop_food_count < MaxSpawn.STOP_FOOD:
-      while True:
-        x, y = random_coords(Map.SIZE - 1)
-        if self._map[y][x].value() == Cell.EMPTY and (x, y) not in first_snake.body and (x, y) not in second_snake.body:
-          self._map[y][x].set_value(Cell.HAS_STOP_FOOD)
-          self._stop_food_count += 1
-          break
-
-    if chance(Probabilities.WALL_SPAWN):
-      while True:
-        x, y = random_coords(Map.SIZE - 1)
-        if self._map[y][x].value() == Cell.EMPTY and (x, y) not in first_snake.body and (x, y) not in second_snake.body:
-          self._map[y][x].set_value(Cell.HAS_WALL)
-          break
+    for spawn in spawns:
+      self._place_object(first_snake, second_snake, *spawn)
